879972bcc7dfc969fd4df16f5341e1c130b2fbb9
M.Kovalevskyi <kovnick1@gmail.com>
2016-08-14 02:05:42 +0300

    remove comments

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 'use strict';
 
-// var metarFetcher = new MetarFetcher();
-// var tafFetcher = new TafFetcher();
-
-// metarFetcher.getData('UKBB').then(function(response) {
-// 	console.log(response)
-// }, function(error) {
-// 	console.error(error);
-// });
-// metarFetcher.getDecodedData('UKBB').then(function(response) {
-// 	console.log(response)
-// }, function(error) {
-// 	console.error(error);
-// });
-// tafFetcher.getData('UKBB').then(function(response) {
-// 	console.log(response)
-// }, function(error) {
-// 	console.error(error);
-// });
-
 module.exports = {
 	MetarFetcher: require('./lib/metarFetcher'),
 	TafFetcher: require('./lib/tafFetcher')
